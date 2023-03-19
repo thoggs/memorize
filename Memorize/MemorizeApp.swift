466fb834dc9e9806a37a8b04fc1f090dc958d947
Thiago Rodrigues <thiagothogs@icloud.com>
2023-03-19 02:25:25 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Thiago Rodrigues on 19/03/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
